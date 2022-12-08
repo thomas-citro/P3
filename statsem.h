014af0890a9574aa6318b90d19bb97f42930c539
thomas-citro <99105760+thomas-citro@users.noreply.github.com>
2022-12-07 22:50:47 -0600

    traversal AFTER changing functions

--- statsem.h
+++ statsem.h
@@ -4,6 +4,10 @@
 void statSemantics(node*, std::string);
 int find(std::string);
 void traverse(node*);
+void processR(node*);
+void processInput(node*);
+void processAssign(node*);
+void processVars(node*, int&);
 void statSemanticsError(std::string, std::string, int);
 
 #endif
